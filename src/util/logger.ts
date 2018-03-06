9f22228e8cb4a7a83be9bdd5bfb3d61a86d9406e
Eetu Sutinen <eetu.sutinen@futurice.com>
2018-03-06 09:58:58 +0200

    logger is always an instance

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -2,22 +2,18 @@ import { createLogger, format, transports } from 'winston';
 
 import config from './config';
 
-let logger: any;
+const level = config.LOGGER_LEVEL;
+const isProd = config.NODE_ENV === 'production';
 
-if (!logger) {
-  const level = config.LOGGER_LEVEL;
-  const isProd = config.NODE_ENV === 'production';
-
-  logger = createLogger({
-    level,
-    format: format.combine(
-      format.colorize({ all: !isProd }),
-      format.simple(),
-    ),
-    transports: [
-      new transports.Console(),
-    ],
-  });
-}
+const logger = createLogger({
+  level,
+  format: format.combine(
+    format.colorize({ all: !isProd }),
+    format.simple(),
+  ),
+  transports: [
+    new transports.Console(),
+  ],
+});
 
 export default logger;
